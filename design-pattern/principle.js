7234984a635e8a80486ed9f7e8aa4c138055323e
李英杰 <yrejgnl11@163.com>
2023-02-08 09:41:06 +0800

    Init project

---
+++ design-pattern/principle.js
@@ -0,0 +1,9 @@
+/**
+ * SOLID设计原则
+ * 
+ * 1. 单一功能原则 Single Responsibility Principle
+ * 2. 开放封闭原则 Opened Closed Principle
+ * 3. 里式替换原则 Liskov Substitution Principle
+ * 4. 接口隔离原则 Interface Segregation Principle
+ * 5. 依赖翻转原则 Dependency Inversion Principle
+ */
